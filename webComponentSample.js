ea663892ee23df852eb14d8a8e626a5009660aff
Amin Raoof <rf.amin@gmail.com>
2024-04-04 18:38:41 +0200

    update test

--- webComponentSample.js
+++ webComponentSample.js
@@ -1,11 +1,26 @@
-
-
 class ResultComponent extends HTMLElement {
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
 
     this._prop = null // Initialize the property
+
+
+    const container = document.createElement('div')
+      container.innerHTML = `
+      <div style="padding: 24px; border: 9px solid blue;">
+      <p>DATA IS: ${this._prop ? this._prop : 0}</p>
+          <div id="resultDiv"></div>
+          <button style="
+          padding: 20px;
+          background-color: lightblue;
+          font-size: larger;
+          font-weight: bold;
+          color: darkblue;
+      " id="catchButton">READY</button>
+          </div>
+        `
+      this.shadowRoot.appendChild(container)
 
     var div = document.createElement("div");
 
