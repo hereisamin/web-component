10f59223b8395bf5866dad1ce32803902d9bbb16
Amin Raoof <rf.amin@gmail.com>
2024-04-04 18:55:20 +0200

    use observedAttributes

--- webComponentSample.js
+++ webComponentSample.js
@@ -1,12 +1,15 @@
 class ResultComponent extends HTMLElement {
+  static get observedAttributes() {
+    return ['prop']; // Add the prop attribute to the list of observed attributes
+  }
+
   constructor () {
     super()
     this.attachShadow({ mode: 'open' })
 
-    this._prop = null // Initialize the property
+    this._prop = this.getAttribute('prop') // Initialize the property from the attribute
 
-
-    const container = document.createElement('div')
+    this.container = document.createElement('div')
       container.innerHTML = `
       <div style="padding: 24px; border: 9px solid blue;">
       <p>DATA IS: ${this._prop ? this._prop : 0}</p>
@@ -20,19 +23,34 @@ class ResultComponent extends HTMLElement {
       " id="catchButton">READY</button>
           </div>
         `
-      this.shadowRoot.appendChild(container)
+      
 
-    var div = document.createElement("div");
+    this.div = document.createElement("div");
+    this.header = document.getElementsByClassName('sticky-header__container')[0]
 
     setInterval(() => {
       div.innerHTML = `Time: ${new Date().toLocaleString('en-US')} ${this._prop ? ', ' + this._prop : ''}`; // Use the property
     }, 1000)
 
-    document.getElementsByClassName('sticky-header__container')[0].appendChild(div)
+    
+  }
+
+  renderElements() {
+    if (this.shadowRoot.contains(this.container)) {
+      this.shadowRoot.removeChild(this.container)
+    }
+
+    if (this.header.contains(this.div)) {
+      this.header.removeChild(this.div)
+    }
+
+    this.shadowRoot.appendChild(this.container)
+    this.header.appendChild(this.div)
   }
 
   set prop(value) { // Define a setter for the property
     this._prop = value
+    this.renderElements()
   }
 
   get prop() { // Define a getter for the property
