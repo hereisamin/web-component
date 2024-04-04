31983d88f04a57cae7ac5d30c25bb66a7fa0351a
Amin Raoof <rf.amin@gmail.com>
2024-04-04 21:40:31 +0200

    add event listener

--- webComponentSample.js
+++ webComponentSample.js
@@ -28,6 +28,8 @@ class ResultComponent extends HTMLElement {
     this.div = document.createElement("div");
     this.header = document.getElementsByClassName('sticky-header__container')[0]
 
+    this.shadowRoot.getElementById('catchButton').addEventListener('click', this.getResult.bind(this))
+
     setInterval(() => {
       this.div.innerHTML = `Time: ${new Date().toLocaleString('en-US')} ${this._prop ? ', ' + this._prop : ''}`; // Use the property
     }, 1000)
@@ -56,6 +58,26 @@ class ResultComponent extends HTMLElement {
   get prop() { // Define a getter for the property
     return this._prop
   }
+
+    async getResult () {
+      try {
+        const response = await fetch(`https://dummyjson.com/products/${this._prop}`)
+        if (!response.ok) {
+          throw new Error('Failed to fetch data')
+        }
+        const data = await response.json()
+
+        this.shadowRoot.getElementById('resultDiv').innerHTML = `
+        <h1>Name: ${data.title}</h1>
+        <h3>Price: ${data.price}</h1>
+        <h3>Description:</h1><p>${data.description}</p>
+        <img src="${data.images[0]}" alt="Girl in a jacket" width="500" height="500">
+        `
+
+      } catch (error) {
+        console.error(error)
+      }
+    }
 }
 
 customElements.define('result-component', ResultComponent)
