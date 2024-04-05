fda5125d1004abf09ba5c17fc9c85cad8c5dcfba
Amin Raoof <rf.amin@gmail.com>
2024-04-05 14:11:23 +0200

    fix component

--- webComponentSample.js
+++ webComponentSample.js
@@ -1,39 +1,34 @@
 class ResultComponent extends HTMLElement {
-  static get observedAttributes() {
-    return ['prop']; // Add the prop attribute to the list of observed attributes
-  }
-
-  constructor () {
-    super()
-    this.attachShadow({ mode: 'open' })
-
-    this._prop = this.getAttribute('prop') // Initialize the property from the attribute
-
-    this.container = document.createElement('div')
-    this.container.innerHTML = 
-    `<div style="padding: 24px; border: 9px solid blue;">
-    <p>Guid is: ${this._prop ? this._prop : 'not available'}</p>
-    </div>`
-
-    this.renderElements()
-  }
-
-  renderElements() {
-    if (this.shadowRoot.contains(this.container)) {
-      this.shadowRoot.removeChild(this.container)
+    static get observedAttributes() {
+        return ['prop']; // Add the prop attribute to the list of observed attributes
     }
 
-    this.shadowRoot.appendChild(this.container)
-  }
+    constructor () {
+        super()
+        this.attachShadow({ mode: 'open' })
 
-  set prop(value) { // Define a setter for the property
-    this._prop = value
-    this.renderElements()
-  }
+        this._prop = this.getAttribute('prop') // Initialize the property from the attribute
 
-  get prop() { // Define a getter for the property
-    return this._prop
-  }
+        this.container = document.createElement('div')
+
+        this.shadowRoot.appendChild(this.container)
+        this.renderElements()
+    }
+
+    renderElements() {
+        this.container.innerHTML = `<div style="padding: 24px; border: 9px solid blue;">
+                                        <p>Guid is: ${this._prop ? this._prop : 'not available'}</p>
+                                    </div>`
+    }
+
+    set prop(value) { // Define a setter for the property
+        this._prop = value
+        this.renderElements()
+    }
+
+    get prop() { // Define a getter for the property
+        return this._prop
+    }
 }
 
 customElements.define('result-component', ResultComponent)
