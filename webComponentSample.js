d8124d17b878117def6d3f19ea7e304af92761bf
Amin Raoof <rf.amin@gmail.com>
2024-04-19 14:52:40 +0200

    Update webComponentSample.js

--- webComponentSample.js
+++ webComponentSample.js
@@ -1,34 +1,67 @@
+
 class ResultComponent extends HTMLElement {
-    static get observedAttributes() {
-        return ['prop']; // Add the prop attribute to the list of observed attributes
-    }
-
     constructor () {
-        super()
-        this.attachShadow({ mode: 'open' })
+      super()
+      this.attachShadow({ mode: 'open' })
 
-        this._prop = this.getAttribute('prop') // Initialize the property from the attribute
+      this._prop = null
+  
+      const container = document.createElement('div')
+      container.innerHTML = `
+      <div style="padding: 24px; border: 9px solid blue;">
+      <p>DATA IS: ${this._prop}</p>
+          <div id="resultDiv"></div>
+          <button style="
+          padding: 20px;
+          background-color: lightblue;
+          font-size: larger;
+          font-weight: bold;
+          color: darkblue;
+      " id="catchButton">READY</button>
+          </div>
+        `
+      this.shadowRoot.appendChild(container)
+  
+      this.shadowRoot.getElementById('catchButton').addEventListener('click', this.getResult.bind(this))
 
-        this.container = document.createElement('div')
+      var div = document.createElement("div");
 
-        this.shadowRoot.appendChild(this.container)
-        this.renderElements()
+      setInterval(() => {
+        div.innerHTML = `Time: ${new Date().toLocaleString('en-US')}`;
+      }, 1000)
+
+      // document.getElementsByClassName('sticky-header__container').style
+      const foo = document.getElementsByClassName('sticky-header__container')[0].appendChild(div)
+      console.log({foo})
+    }
+  
+    async getResult () {
+      try {
+        const response = await fetch(`https://dummyjson.com/products/${this._prop}`)
+        if (!response.ok) {
+          throw new Error('Failed to fetch data')
+        }
+        const data = await response.json()
+
+        this.shadowRoot.getElementById('resultDiv').innerHTML = `
+        <h1>Name: ${data.title}</h1>
+        <h3>Price: ${data.price}</h1>
+        <h3>Description:</h1><p>${data.description}</p>
+        <img src="${data.images[0]}" alt="Girl in a jacket" width="500" height="500">
+        `
+        
+      } catch (error) {
+        console.error(error)
+      }
     }
 
-    renderElements() {
-        this.container.innerHTML = `<div style="padding: 24px; border: 9px solid blue;">
-                                        <p>Guid is: ${this._prop ? this._prop : 'not available'}</p>
-                                    </div>`
+    set prop(value) {
+      this._prop = value
     }
-
-    set prop(value) { // Define a setter for the property
-        this._prop = value
-        this.renderElements()
+  
+    get prop() {
+      return this._prop
     }
-
-    get prop() { // Define a getter for the property
-        return this._prop
-    }
-}
-
-customElements.define('result-component', ResultComponent)
+  }
+  
+  customElements.define('result-component', ResultComponent)
