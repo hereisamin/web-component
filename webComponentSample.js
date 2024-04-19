0d9a541a9f903432ebee8b5b1dc2c650578a113d
Amin Raoof <rf.amin@gmail.com>
2024-04-19 15:40:41 +0200

    Update webComponentSample.js

--- webComponentSample.js
+++ webComponentSample.js
@@ -1,15 +1,18 @@
-
 class ResultComponent extends HTMLElement {
-    constructor () {
-      super()
-      this.attachShadow({ mode: 'open' })
+  static get observedAttributes() {
+    return ['prop']; // Add the prop attribute to the list of observed attributes
+  }
 
-      this._prop = null
-  
-      const container = document.createElement('div')
-      container.innerHTML = `
+  constructor () {
+    super()
+    this.attachShadow({ mode: 'open' })
+
+    this._prop = this.getAttribute('prop') // Initialize the property from the attribute
+
+    this.container = document.createElement('div')
+    this.container.innerHTML = `
       <div style="padding: 24px; border: 9px solid blue;">
-      <p>DATA IS: ${this._prop}</p>
+      <p>DATA IS: ${this._prop ? this._prop : 0}</p>
           <div id="resultDiv"></div>
           <button style="
           padding: 20px;
@@ -20,21 +23,42 @@ class ResultComponent extends HTMLElement {
       " id="catchButton">READY</button>
           </div>
         `
-      this.shadowRoot.appendChild(container)
-  
-      this.shadowRoot.getElementById('catchButton').addEventListener('click', this.getResult.bind(this))
+      
 
-      var div = document.createElement("div");
+    this.div = document.createElement("div");
+    this.header = document.getElementsByClassName('sticky-header__container')[0]
 
-      setInterval(() => {
-        div.innerHTML = `Time: ${new Date().toLocaleString('en-US')}`;
-      }, 1000)
+    setInterval(() => {
+      this.div.innerHTML = `Time: ${new Date().toLocaleString('en-US')} ${this._prop ? ', ' + this._prop : ''}`; // Use the property
+    }, 1000)
 
-      // document.getElementsByClassName('sticky-header__container').style
-      const foo = document.getElementsByClassName('sticky-header__container')[0].appendChild(div)
-      console.log({foo})
+    this.renderElements()
+  }
+
+  renderElements() {
+    if (this.shadowRoot.contains(this.container)) {
+      this.shadowRoot.removeChild(this.container)
     }
-  
+
+    if (this.header.contains(this.div)) {
+      this.header.removeChild(this.div)
+    }
+
+    this.shadowRoot.appendChild(this.container)
+    this.header.appendChild(this.div)
+
+    this.shadowRoot.getElementById('catchButton').addEventListener('click', this.getResult.bind(this))
+  }
+
+  set prop(value) { // Define a setter for the property
+    this._prop = value
+    this.renderElements()
+  }
+
+  get prop() { // Define a getter for the property
+    return this._prop
+  }
+
     async getResult () {
       try {
         const response = await fetch(`https://dummyjson.com/products/${this._prop}`)
@@ -49,19 +73,11 @@ class ResultComponent extends HTMLElement {
         <h3>Description:</h1><p>${data.description}</p>
         <img src="${data.images[0]}" alt="Girl in a jacket" width="500" height="500">
         `
-        
+
       } catch (error) {
         console.error(error)
       }
     }
+}
 
-    set prop(value) {
-      this._prop = value
-    }
-  
-    get prop() {
-      return this._prop
-    }
-  }
-  
-  customElements.define('product-loader', ResultComponent)
+customElements.define('product-loader', ResultComponent)
